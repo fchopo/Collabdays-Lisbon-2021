f9e9db741c1477969be199104a26ff8abbd19803
Ferran Chopo <fchopo@hotmail.com>
2021-11-15 16:13:22 +0100

    Initial commit

---
+++ demos/TvMazeSearchTeamsExtension/Models/CardResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TvMazeSearchTeamsExtension.Models
+{
+    public class CardResponse
+    {
+        public string Title { get; set; }
+        public Int32 Rating { get; set; }
+    }
+}
